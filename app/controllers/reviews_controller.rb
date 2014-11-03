16d111cceca9dec18f7adef9a1702b5a09f84024
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-03 19:47:36 +0000

    Reviews can be added and displayed.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,11 +8,11 @@ class ReviewsController < ApplicationController
   def create
   	@restaurant = Restaurant.find(params[:restaurant_id])
   	@restaurant.reviews.create(review_params)
-  	redirect_to restaurant_path
+  	redirect_to restaurants_path
   end
 
   def review_params
-  	params.require(:review).permit(:thoughs, :rating)
+  	params.require(:review).permit(:thoughts, :rating)
   end
 
 end
